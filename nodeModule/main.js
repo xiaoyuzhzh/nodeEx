a9aeb17cbaed842df02e2fdada2c7e9916b253ca
xiaoyuzhzh <xiaoyuzhzh@163.com>
2015-01-26 14:38:47 +0800

    注释修改

--- nodeModule/main.js
+++ nodeModule/main.js
@@ -31,9 +31,8 @@ var a = require('./a.js');
 //    console.log("exported eventEmitter emited a event2.");
 //})
 
-//模块的延迟赋值将不会起作用，官网的api上的例子是直接赋值了exports.我的理解是require()方法返回的是module下的exports所指的对象。所以，在原模块中任何延迟的赋值本质上改变了exports的对象。所以修改是无意义的。而我的版本则给exports所指的对象创建新的属性ob并且给ob赋值。可以发现ob被成功的被main.js中的delay调用出来了。当回调喊出持有正确的exports对象的时候应该是可以异步修改exports对象的值的。
-
-//应该这样理解原版的话，修改module下的exports的实例的过程，在require()方法结束之后就不会再有效果了。加上module对象的缓存，会导致所有指向同一个module文件的只会执行一次
+//模块的延迟赋值将不会起作用，官网的api上的例子是直接赋值了exports.我的理解是require()方法返回的是module下的exports所指的对象。所以，在原模块中任何延迟的赋值本质上改变了exports的对象。所以修改是无意义的。而我的版本则给exports所指的对象创建新的属性ob并且给ob赋值。可以发现ob被成功的被main.js中的delay调用出来了。当回调函数持有正确的exports对象的时候应该是可以异步修改exports对象的值的。
+//应该这样理解原版的话，修改module下的exports的实例的过程，在require()方法结束之后就不会再有效果了。加上module对象的缓存，会导致所有指向同一个module文件的只会执行一次。注：可以从require.cache删除缓存的modules,然后让别的require reload module。
 //var delay = require('./assign_delay');
 //setTimeout(function(){
 //               console.log(delay.ob.a);
